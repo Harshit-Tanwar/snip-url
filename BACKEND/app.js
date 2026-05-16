6932229178325535359481e42877d7c911931108
Harshit Tanwar <harshittanwar0@gmail.com>
2026-05-16 15:41:32 +0530

    frontend origin

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -16,7 +16,7 @@ dotenv.config("./.env")
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173', // your React app
+    origin: 'https://snip-url-weld.vercel.app/', // your React app
     credentials: true // 👈 this allows cookies to be sent
 }));
 

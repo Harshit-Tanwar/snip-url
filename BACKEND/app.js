a7149f6c2f9db461781987afc36a391a465a1158
Harshit Tanwar <harshittanwar0@gmail.com>
2026-05-17 20:17:51 +0530

    app url update

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -16,7 +16,7 @@ dotenv.config("./.env")
 const app = express();
 
 app.use(cors({
-    origin: 'https://snip-url-weld.vercel.app', // your React app
+    origin: import.meta.env.APP_URL, // your React app
     credentials: true // 👈 this allows cookies to be sent
 }));
 
